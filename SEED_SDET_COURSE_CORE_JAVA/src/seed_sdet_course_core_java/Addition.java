6b8e1a54f0592e9a642ada795b16c610b66056b8
itsvishwa23 <itsvishwa23@gmail.com>
2024-05-11 11:51:20 +0530

    Day_1

---
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Addition.java
@@ -0,0 +1,15 @@
+package seed_sdet_course_core_java;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=10;
+		int sum=a+b;
+		System.out.println("The Sum is:" +sum+" Thanks");
+		
+
+	}
+
+}
