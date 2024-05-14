5427f6eec87cb4e5df4eb53832e2891148623b2c
itsvishwa23 <itsvishwa23@gmail.com>
2024-05-14 11:16:55 +0530

    :tada:

---
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_3_Calculator.java
@@ -0,0 +1,36 @@
+package seed_sdet_course_core_java;
+
+public class Day_3_Calculator {
+	
+	public int multiply(int a, int b, int c) {
+	int result=a*b*c;
+	System.out.println(result);
+	return result;
+		
+	}
+
+		public double  multiply(double a, double  b, double  c) {
+		double  result=a*b*c;
+		System.out.println(result);
+		return result;
+		
+		
+	}
+		
+		public float  multiply(float a, float  b, float  c) {
+			float  result=a*b*c;
+			System.out.println(result);
+			return result;
+			
+			
+		}
+		
+		public int multiply(int a, int b, int c, int d) {
+			int result=a*b*c*d;
+			System.out.println(result);
+			return result;
+			
+				
+			}
+		
+}
---
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_3_Calculator_Main.java
@@ -0,0 +1,15 @@
+package seed_sdet_course_core_java;
+
+public class Day_3_Calculator_Main {
+
+	public static void main(String[] args) {
+		
+		Day_3_Calculator d1= new Day_3_Calculator();
+		d1.multiply(5.35, 5.35, 5.35);
+		d1.multiply(1.5, 2.5, 3.5);
+		d1.multiply(1, 2, 3);
+		d1.multiply(1, 2, 3, 4);
+
+	}
+
+}
