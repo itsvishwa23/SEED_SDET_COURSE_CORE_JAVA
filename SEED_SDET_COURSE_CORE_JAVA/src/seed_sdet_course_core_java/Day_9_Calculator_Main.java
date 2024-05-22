c793218eee312790e8b08345936f9af5b7a0022a
itsvishwa23 <itsvishwa23@gmail.com>
2024-05-22 12:32:06 +0530

    :tada:

---
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_9_Calculator_Main.java
@@ -0,0 +1,53 @@
+package seed_sdet_course_core_java;
+
+public class Day_9_Calculator_Main {
+
+	public static void add(int a, int b) {
+		int sum = a + b;
+		System.out.println("The addition is " + sum);
+	}
+
+	public static void subtract(int a, int b) {
+		int difference = a - b;
+		System.out.println("The subtraction is " + difference);
+	}
+
+	public static void multiply(int a, int b) {
+		int product = a * b;
+		System.out.println("The multiplication is " + product);
+	}
+
+	public static void divide(int a, int b) {
+		if (b != 0) {
+			int quotient = a / b;
+			System.out.println("The division is " + quotient);
+		} else {
+			System.out.println("Division by zero is not allowed.");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		String calc = "Addition";
+
+		switch (calc) {
+		case "Addition":
+			add(10, 20);
+			break;
+		case "Subtraction":
+			subtract(10, 20);
+			break;
+		case "Multiplication":
+			multiply(10, 20);
+			break;
+		case "Division":
+			divide(10, 20);
+			break;
+		default:
+			System.out.println("Invalid Input.");
+			break;
+		}
+
+	}
+
+}
