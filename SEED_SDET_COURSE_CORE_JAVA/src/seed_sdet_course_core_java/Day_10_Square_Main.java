42bdf909de580fe88fdfe09c4d837350a6135ea9
itsvishwa23 <itsvishwa23@gmail.com>
2024-05-23 20:21:38 +0530

    :tada:

--- SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_10_Square_Main.java
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_10_Square_Main.java
@@ -5,8 +5,8 @@ public class Day_10_Square_Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Day_10_Square d1 = new Day_10_Square(-2);
 		// Day_10_Square d1 = new Day_10_Square(-2);
+		Day_10_Square d1 = new Day_10_Square(-2);
 		d1.sqr();
 		System.out.println(d1);
 
