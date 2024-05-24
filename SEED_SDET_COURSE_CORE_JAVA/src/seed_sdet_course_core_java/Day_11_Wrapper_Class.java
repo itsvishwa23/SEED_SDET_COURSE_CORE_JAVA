1445057c2404f7543635655e6ba550ac33a43dd4
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-05-24 19:57:40 +0530

    Create Day_11_Wrapper_Class.java
    :100:

---
+++ SEED_SDET_COURSE_CORE_JAVA/src/seed_sdet_course_core_java/Day_11_Wrapper_Class.java
@@ -0,0 +1,15 @@
+package seed_sdet_course_core_java;
+
+public class Day_11_Wrapper_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		Integer c = a;
+		Integer b = 10;
+		String myString = b.toString();
+		System.out.println(myString);
+
+	}
+
+}
